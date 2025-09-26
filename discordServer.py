0223ddf81fe928ab548e6e24b132bbcfad792ef5
Sakaduki <owcatomts@gmail.com>
2025-09-26 17:59:24 +0900

    AdddebugCode-contentType

--- discordServer.py
+++ discordServer.py
@@ -17,7 +17,7 @@ async def on_message(message:discord.Message):
         return
     if len(message.attachments) != 0:
         for attachment in message.attachments:
-            print("attachments_detect!")
+            print(f"attachments_detect! type:{attachment.content_type}")
             if attachment.content_type=="text/csv":
                 attachment.save("temp/reciveCsvFile.csv")
                 updateDataDict("temp/reciveCsvFile.csv")
