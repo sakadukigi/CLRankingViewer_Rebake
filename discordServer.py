91cd0dbf704b679c7f6321b48ab3971cf4ea4c7f
Sakaduki <owcatomts@gmail.com>
2025-09-26 17:42:41 +0900

    hotfix-2nd

--- discordServer.py
+++ discordServer.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
-client = discord.Client(intents=discord.Intents.all)
+client = discord.Client(intents=discord.Intents.all())
 
 dukiGeneral.TryMakeDir("data")
 dukiGeneral.TryMakeDir("temp")
